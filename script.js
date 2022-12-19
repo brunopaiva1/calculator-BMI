b239ea1b263c43406a4c0b4761c848e474179dec
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-18 23:58:31 -0300

    .

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ form.addEventListener('submit', function(e) {
     const message = (document.querySelector('#message'));
 
     if((height *** '') ||(height < 0) ||(isNaN(height))){
-        results.innerHTML = "Por favor forneça um valo válido";
+        results.innerHTML = "Por favor forneça um valor válido";
 
     }else if((weight *** '') || weight < 0 || isNaN(weight)){
+        results.innerHTML = "Por favor forneça um peso válido";
+
+    }else{
+        const bmi = (weight /((height * height) / 10000)).toFixed(2);
+        results.innerHTML = '<span>$(bmi)</span>'
+
+        if(bmi < 18.6) {
+            message.innerHTML = "Você estar baixo do peso";
+        } else if(bmi > 18.6 && bmi < 24.9){
+            message.innerHTML = "Normal"
+        }else if(bmi > 24.9){
+            message.innerHTML = "Você estar acima do peso"
+        }
     }
-
-
-
-
-})
+});
