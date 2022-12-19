4cb452b887e613525074b1018caeffb6b7f02cfc
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-19 00:26:43 -0300

    .

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ form.addEventListener('submit', function (e) {
     const results = (document.querySelector('#results'));
     const message = (document.querySelector('#message'));
 
-    if((height === '') || (height < 0) || (isNaN(height))){
+    if ((height === '') || (height < 0) || (isNaN(height))){
         results.innerHTML = "Por favor forneça um valor válido";
 
-    }else if(weight === '' || weight < 0 || isNaN(weight)){
+    }else if (weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = "Por favor forneça um peso válido";
 
     }else{
-        const IMC = (weight /((height * height) / 10000)).toFixed(2);
+        const IMC = (weight / ((height * height) / 10000)).toFixed(2);
         results.innerHTML = '<span>${IMC}</span>'
 
         if(IMC < 18.6) {
