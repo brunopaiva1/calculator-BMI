499f344079609b41c67a24c45748ce40aa3107a6
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-19 14:06:23 -0300

    .

--- script.js
+++ script.js
@@ -5,26 +5,29 @@ form.addEventListener('submit', function (e) {
 
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
-    const results = (document.querySelector( '#results'));
-    const message = (document.querySelector('#message'));
+    const results = document.querySelector('#results');
+    const message = document.querySelector('#message');
 
-    if ((height === '') || (height < 0) || (isNaN(height))){
+    if ((height === '') || (height < 0) || (isNaN(height))) {
+        //NaN !== NaN
         results.innerHTML = "Please provide a valid height";
 
-    }else if (weight === '' || weight < 0 || isNaN(weight)){
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
         results.innerHTML = "Please provide a valid weight";
-
-    }else{
+    } else {
+        // Calculate BMI
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        // Display the results
+        results.innerHTML = `<span>${bmi}</span>`
 
-        results.innerHTML = '<span>${bmi}</span>'
-
-        if(bmi < 18.6) {
+        // Display End Message
+        if (bmi < 18.6) {
             message.innerHTML = "You are underweight";
-        } else if(bmi > 18.6 && bmi < 24.9){
+        } else if (bmi > 18.6 && bmi < 24.9) {
             message.innerHTML = "Normal"
-        } else if(bmi > 24.9){
-            message.innerHTML = "You are overweight"
+        } else if (bmi > 24.9) {
+            message.innerHTML = "You are Overweight"
         }
     }
 });
+
