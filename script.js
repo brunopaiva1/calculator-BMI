ab6e2cf289fdfc8023fad91567963f5c0d846e87
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-19 00:22:48 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const height = parseInt(document.querySelector('#height').value);
@@ -6,21 +6,21 @@ form.addEventListener('submit', function(e) {
     const results = (document.querySelector('#results'));
     const message = (document.querySelector('#message'));
 
-    if((height === '') ||(height < 0) ||(isNaN(height))){
+    if((height === '') || (height < 0) || (isNaN(height))){
         results.innerHTML = "Por favor forneça um valor válido";
 
-    }else if((weight === '') || weight < 0 || isNaN(weight)){
+    }else if(weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = "Por favor forneça um peso válido";
 
     }else{
-        const bmi = (weight /((height * height) / 10000)).toFixed(2);
-        results.innerHTML = '<span>$(bmi)</span>'
+        const IMC = (weight /((height * height) / 10000)).toFixed(2);
+        results.innerHTML = '<span>${IMC}</span>'
 
-        if(bmi < 18.6) {
+        if(IMC < 18.6) {
             message.innerHTML = "Você estar baixo do peso";
-        } else if(bmi > 18.6 && bmi < 24.9){
+        } else if(IMC > 18.6 && IMC < 24.9){
             message.innerHTML = "Normal"
-        }else if(bmi > 24.9){
+        }else if(IMC > 24.9){
             message.innerHTML = "Você estar acima do peso"
         }
     }
